18f1cc20aab4cacbd56989d8794678c1dee7fdd5
Roman Pushkin <roman@breakthrough.com>
2016-03-28 20:06:51 -0700

    Add esdoc comments

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import { PluginError } from 'gulp-util';
 
 const PLUGIN_NAME = 'gulp-easy-react';
 
+/**
+ * Transforms ES6/JSX file(s) to browserified and babelified bundle
+ */
 class EasyReact {
+
+  /**
+   * Creates EasyReact object
+   *
+   * @param  {object} [options]
+   * @param  {string} [options.file] - Single ES6/JSX file
+   * @param  {string} [options.files] - Mask for multiple ES6/JSX files
+   */
   constructor(options) {
     if (!options.file) {
       throw new PluginError(PLUGIN_NAME, 'Missing incoming file parameter!');
     }
   }
 
+  /**
+   * Pipes out browserified, babelified and bundled React.js code
+   *
+   * @param  {string} file - Output file name
+   * @return {object} pipe - Gulp pipe
+   */
   to(file) {
   }
 }
