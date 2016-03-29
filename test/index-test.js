0ea486dec7bb25d9f28b3d5ecf618fe4ecfb1e13
Roman Pushkin <roman@breakthrough.com>
2016-03-28 17:00:49 -0700

    Test constuctor and methods to be defined

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,12 @@
 import test from 'ava';
 import EasyReact from '../src/index.js';
 
-test('foo', t => {
-    t.pass();
+test.cb('Should have methods and constructor defined', t => {
+  var easyReact = new EasyReact({
+    file: 'dummy.txt'
+  });
+
+  t.ok(easyReact);
+  t.ok(typeof easyReact.to === 'function')
+  t.end();
 });
